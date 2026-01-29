2e2779f31172c2aa8ee48071ef5e407b0e9349b2
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-01-29 04:07:18 +0000

    python

---
+++ Function/basic.py
@@ -0,0 +1,14 @@
+#function definition
+# def calc_sum(a,b):
+#     return a+b
+
+# print(calc_sum(2,5))
+
+# print(calc_sum(4,5))
+
+# print(calc_sum(6,10))
+
+def print_hello():
+    print("Hello")
+
+print_hello()    
