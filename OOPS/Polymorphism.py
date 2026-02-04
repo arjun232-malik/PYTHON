b2344395ead746db967f527678ddb9cbda381c12
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-02-04 18:44:29 +0000

    python

---
+++ OOPS/Polymorphism.py
@@ -0,0 +1,21 @@
+class ComplexNum:
+    def __init__(self,real,img):
+        self.real=real
+        self.img=img
+
+    def showNum(self):
+        print(self.real,"i +",self.img,"j")
+
+    def add(self,num2):
+        newReal=self.real+num2.real
+        newImg=self.img+num2.img
+        return ComplexNum(newReal,newImg)
+
+num1=ComplexNum(3,2)
+num1.showNum()
+
+num2=ComplexNum(5,7)
+num2.showNum()
+
+num3=num1.add(num2)
+num3.showNum()
--- OOPS/propertyDecorators.py
+++ OOPS/propertyDecorators.py
@@ -3,13 +3,13 @@ class Student:
         self.phy=phy
         self.chem=chem
         self.math=math
-        self.percentage=str((self.phy+self.chem+self.math)/3) + "%"
-        
-        def chngemarks
+
+    @property
+    def percentage(self):
+        return str((self.phy+self.chem+self.math)/3) + "%"
 
 s1=Student(98,78,92)
 print(s1.percentage)
 
 s1.chem=87
-print(s1.chem)
 print(s1.percentage)
