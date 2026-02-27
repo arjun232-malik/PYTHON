921f7cc79999798711de6c5c7adbf4dd462af220
arjun232-malik <arjunsiddhumalik@gmail.com>
2026-02-27 14:05:57 +0530

    python

--- MINI PROJECTS/ToDolist.py
+++ MINI PROJECTS/ToDolist.py
@@ -1,16 +1,18 @@
-ch="To Do List:\n"
-ch=ch+"1. Wake up at 6 am\n"
-ch=ch+"2. 10 push up everyday\n"
-ch=ch+"3. Finish one lecture of C++ DSA everyday\n"
-ch=ch+"4. Do revision of all lectures notes\n"
+ch=['To Do List:\n','1. Wake up at 6 am\n','2. 10 push up everyday\n','3. Finish one lecture of C++ DSA everyday\n','4. Do revision of notes\n']
+lst=str.join(" ",ch)
 
 while True:
-    ask=str(input("(S):SHOW THE LIST OR (A):WANT TO SOMETHING OR (E):TO EXIT\n"))
+    ask=str(input("(S):SHOW THE LIST OR (A):WANT TO ADD SOMETHING OR (D):WANT TO DELETE THE LINE OR (E):TO EXIT\n"))
 
     if(ask=='S'):
-        print(ch)
+        print(lst)
     elif(ask=='A'):
-        ch=ch+str(input(""))
+        ch.append(str(input("")))
+        lst=str.join(" ",ch)
+        print('\n')
+    elif(ask=='D'):
+        ch.pop(int(input("ENTER THE NUMBER OF LINE YOU WANT TO DELETE:")))
+        lst=str.join(" ",ch)
     elif(ask=='E'):
         print("BYE :)")
         break
