217448e2f913bc3977a9214ab4a7fe2528fb2a68
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-05-25 15:42:57 +0530

    python

---
+++ 03_Lists/04_loop.py
@@ -0,0 +1,8 @@
+# squared_number=[]
+
+# for i in range(1,11):
+#     squared_number.append(i**2)
+# print(squared_number)    
+
+squared_number=[i**2 for i in range(1,11)]
+print(squared_number)
---
+++ 05_Loops/enumerateFunction.py
@@ -0,0 +1,8 @@
+famous_car_brands = ["ferrari","lamborghini","mercedes"]
+# index=0
+# for item in famous_car_brands:
+#     print(index,item)
+#     index+=1
+
+for index,item in enumerate(famous_car_brands):
+    print(index,item)
