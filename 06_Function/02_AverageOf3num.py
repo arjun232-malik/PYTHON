bd7b0b5afbb520d7dec8edfcc3df5ecb17ae918c
arjun232--malik <arjunsiddhumalik@gmail.com>
2026-05-25 15:42:28 +0530

    python

--- 06_Function/02_AverageOf3num.py
+++ 06_Function/02_AverageOf3num.py
@@ -1,7 +1,8 @@
 def avgOf3num(a,b,c): # user-define function
+    """Calculates the average of three numbers"""
     sum=a+b+c
     avg=sum/3
     print(avg) # user_define function
-    return sum
+    return avg
 
 avgOf3num(23,56,18)
